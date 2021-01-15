519dfcbbc49bfe7464679ceb1346a1de961c7e89
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-15 10:15:33 +0000

    Bump version v0.0.0 -> v0.1.0

--- dict2css/__init__.py
+++ dict2css/__init__.py
@@ -39,7 +39,7 @@ from domdf_python_tools.words import TAB
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.0.0"
+__version__: str = "0.1.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["dumps", "dump", "CSSSerializer", "StyleSheet", "em", "make_style", "px", "rem", "Style", "IMPORTANT"]
